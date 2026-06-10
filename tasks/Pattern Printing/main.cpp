5632103286acbdccd95032f5b938193561fadc98
Yana Beleiovych <208520423+Yanylyya@users.noreply.github.com>
2026-06-10 21:38:22 +0200

    Add pattern printing program in main.cpp

---
+++ tasks/Pattern Printing/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "\n\tEnter number: "; cin >> n;
+	
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <=i; j++){ // for(int j = 1; j <= n; j++){
+			cout << "* ";
+		}
+		cout << endl;
+	}
+		
+return 0;
+}
